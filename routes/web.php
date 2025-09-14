7a6511dfb58bc63891a5db6c32e99e97ca38f0ab
YassDev_989u <148916200+Yassine898@users.noreply.github.com>
2025-09-15 00:49:33 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Cookie;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('/sanctum/csrf-cookie', function (Request $request) {
+    return response()->json()->withCookie(
+        new Cookie(
+            'XSRF-TOKEN',
+            $request->session()->token(),
+            120, // minutes
+            '/',
+            null,
+            config('session.secure_cookie', false), // secure = true if using https
+            false, // ❌ HttpOnly = false → frontend can read it
+            false,
+            config('session.same_site', 'lax')
+        )
+    );
+});
